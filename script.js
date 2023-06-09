cb51071f28732575a99e3c1aecd9f959437720e9
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-06-09 18:09:37 +0100

    added js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ for(let i = 0; i < SQUARES; i++){
     const square = document.createElement('div');
     square.classList.add('square');
 
+    square.addEventListener('mouseover', () => ReadableStreamDefaultController(square))
+
+    square.addEventListener('mouseover', () => ReadableStreamDefaultController(square))
+
     container.appendChild(square);
 }
 
